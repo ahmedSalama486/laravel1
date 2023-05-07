8d9e9fdec0a5669899e327b455ef729c9ff2b4cf
ahmedSalama486 <as7097931@gmail.com>
2023-05-07 11:06:27 +0200

    added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Department;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+Route::get('departments', function () {
+    $departments= Department::get();
+    return view('departments',[ 'departments'=> $departments ]);
 });
